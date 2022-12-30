0b9b635633d64bb8feeae140c7749017851c9dd3
Steffen Marbach <steffen.marbach@student.uni-luebeck.de>
2022-12-30 03:02:52 +0100

    add Button is now an Icon and at Top in Select Screen

--- src/app/src/main/java/com/ispgr5/locationsimulator/presentation/select/SelectScreen.kt
+++ src/app/src/main/java/com/ispgr5/locationsimulator/presentation/select/SelectScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,32 +27,40 @@ fun SelectScreen(
     viewModel: SelectViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Column(
-        Modifier
-            .padding(15.dp)
-            .fillMaxSize()
+     Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            //for all configurations in state we create a SelectConfigurationButton
-            items(state.configurations) { configuration ->
-                SelectConfigurationButton(
-                    configuration = configuration
-                ) {
-                    viewModel.onEvent(SelectEvent.SelectedConfiguration(configuration))
-                    //TODO navController.navigate("startScreen?configurationId=${configuration.id}") navigate to StartScreen with the Selected Configuration
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-            }
-            //TODO add Button to delete Configurations from list
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = {
-                navController.navigate(route = "editScreen")
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.End
         ) {
-            Text(text = "Add")
+            Button(
+                onClick = {
+                    navController.navigate(route = "editScreen")
+                }
+            ) {
+                Icon(Icons.Filled.Add,"")
+            }
+        }
+        Column(
+            Modifier
+                .padding(15.dp)
+                .fillMaxSize()
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                //for all configurations in state we create a SelectConfigurationButton
+                items(state.configurations) { configuration ->
+                    SelectConfigurationButton(
+                        configuration = configuration
+                    ) {
+                        viewModel.onEvent(SelectEvent.SelectedConfiguration(configuration))
+                        //TODO navController.navigate("startScreen?configurationId=${configuration.id}") navigate to StartScreen with the Selected Configuration
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                }
+                //TODO add Button to delete Configurations from list
+            }
         }
     }
 }
