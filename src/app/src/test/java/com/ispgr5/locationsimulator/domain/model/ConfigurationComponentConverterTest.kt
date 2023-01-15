3d6c62d3df38e91bf80c7cdeac17e169edb9ce95
Steffen Marbach <steffen.marbach@student.uni-luebeck.de>
2023-01-15 12:12:13 +0100

    added Test for the Configuration Component Type Converter

---
+++ src/app/src/test/java/com/ispgr5/locationsimulator/domain/model/ConfigurationComponentConverterTest.kt
@@ -0,0 +1,51 @@
+package com.ispgr5.locationsimulator.domain.model
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Test class for the Configuration Component Converter
+ */
+class ConfigurationComponentConverterTest {
+
+    private val testCompList: List<ConfigComponent> = listOf(
+        Vibration(
+            minStrength = 2,
+            maxStrength = 5,
+            minPause = 3,
+            maxPause = 8,
+        ),
+        Sound(
+            source = "soundSource",
+            minVolume = 3,
+            maxVolume = 7,
+            minPause = 1,
+            maxPause = 9,
+            isRandom = true
+        )
+    )
+
+    private val testCompListStr: String = "[" +
+            "{\"comp_type\":\"Vibration\",\"minStrength\":2,\"maxStrength\":5,\"minPause\":3,\"maxPause\":8}," +
+            "{\"comp_type\":\"Sound\",\"source\":\"soundSource\",\"minVolume\":3,\"maxVolume\":7,\"minPause\":1,\"maxPause\":9,\"isRandom\":true}" +
+            "]"
+
+    /**
+     * test that the json string is the right String from the component List
+     */
+    @ExperimentalSerializationApi
+    @Test
+    fun componentListToString() {
+        assertEquals(testCompListStr, ConfigurationComponentConverter().componentListToString(testCompList))
+    }
+
+    /**
+     * test that the component list is the right when parsing the json String
+     */
+    @ExperimentalSerializationApi
+    @Test
+    fun componentStrToComponentList() {
+        assertEquals(testCompList, ConfigurationComponentConverter().componentStrToComponentList(testCompListStr))
+    }
+}
