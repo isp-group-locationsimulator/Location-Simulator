f36a9d94adc7f18cfed80ce7bcf2d560ba4a9ec3
Steffen Marbach <steffen.marbach@student.uni-luebeck.de>
2023-02-06 03:25:10 +0100

    removed the LocationApp Context memory leak warning

--- src/app/src/main/java/com/ispgr5/locationsimulator/LocationApp.kt
+++ src/app/src/main/java/com/ispgr5/locationsimulator/LocationApp.kt
@@ -1,26 +1,10 @@
 package com.ispgr5.locationsimulator
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 /**
  * This class is needed for the AppModule for Data Injection
  */
 @HiltAndroidApp
-class LocationApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        context = this
-    }
-
-    /**
-     * This companion object enables the rest of the project to access context.
-     * TODO: I am not sure that this is a good solution. Should be checked and changed eventually. Memory leak?
-     */
-    companion object {
-        var context: Context? = null
-            internal set
-    }
-}
-
+class LocationApp : Application()
