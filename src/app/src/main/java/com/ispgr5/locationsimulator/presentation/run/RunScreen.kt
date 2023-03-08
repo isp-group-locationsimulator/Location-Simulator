c9cdc2899cd8c71e3416566cc79685b22186faa7
Max Henning Junghans <MaxHenningJunghans@gmail.com>
2023-03-08 15:53:22 +0100

    Updated the Run-Scene

--- src/app/src/main/java/com/ispgr5/locationsimulator/presentation/run/RunScreen.kt
+++ src/app/src/main/java/com/ispgr5/locationsimulator/presentation/run/RunScreen.kt
@@ -1,10 +1,15 @@
 package com.ispgr5.locationsimulator.presentation.run
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.ispgr5.locationsimulator.R
@@ -19,10 +24,19 @@ fun RunScreen(
     stopServiceFunction : () -> Unit,
     viewModel: RunViewModel = hiltViewModel()
 ) {
-    Button(onClick = {
-        viewModel.onEvent(RunEvent.StopClicked(stopServiceFunction))
-        navController.navigateUp()
-    }) {
-        Text(text = stringResource(id = R.string.run_stop))
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            modifier = Modifier.width(200.dp).height(120.dp),
+            onClick = {
+            viewModel.onEvent(RunEvent.StopClicked(stopServiceFunction))
+            navController.navigateUp()
+        }) {
+            Text(text = stringResource(id = R.string.run_stop), fontSize = 30.sp)
+        }
     }
+
 }
