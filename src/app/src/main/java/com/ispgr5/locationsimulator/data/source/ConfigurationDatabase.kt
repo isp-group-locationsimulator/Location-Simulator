d2999e6735fd2c378fe75481aab04c7487ed32c4
Steffen Marbach <steffen.marbach@student.uni-luebeck.de>
2023-03-14 22:57:55 +0100

    Fixed Database error by increasing the Database Version number.

--- src/app/src/main/java/com/ispgr5/locationsimulator/data/source/ConfigurationDatabase.kt
+++ src/app/src/main/java/com/ispgr5/locationsimulator/data/source/ConfigurationDatabase.kt
@@ -11,7 +11,7 @@ import com.ispgr5.locationsimulator.domain.model.ConfigurationComponentConverter
  */
 @Database(
     entities = [Configuration::class],
-    version = 6,
+    version = 7,
     exportSchema = false
 )
 @TypeConverters(ConfigurationComponentConverter::class)
