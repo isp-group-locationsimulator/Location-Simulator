6af931b52224f1bda78732e53524dbdc78abe2f4
Joshua Wiedekopf <joshua@wiedekopf.net>
2023-11-19 22:19:29 +0100

    beta 1.1.1-beta2

--- src/app/src/main/java/com/ispgr5/locationsimulator/presentation/homescreen/InfoScreen.kt
+++ src/app/src/main/java/com/ispgr5/locationsimulator/presentation/homescreen/InfoScreen.kt
@@ -17,10 +17,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.ispgr5.locationsimulator.BuildConfig
 import com.ispgr5.locationsimulator.R
 import com.ispgr5.locationsimulator.presentation.universalComponents.TopBar
 
@@ -29,51 +31,71 @@ import com.ispgr5.locationsimulator.presentation.universalComponents.TopBar
  */
 @Composable
 fun InfoScreen(
-	navController: NavController,
-	scaffoldState: ScaffoldState
+    navController: NavController,
+    scaffoldState: ScaffoldState
 ) {
-	Scaffold(
-		scaffoldState = scaffoldState,
-		topBar = { TopBar(navController, stringResource(id = R.string.ScreenInfo)) },
-		content = {
-			Column(
-				modifier = Modifier
-					.fillMaxSize()
-					.padding(30.dp + it.calculateTopPadding())
-					.verticalScroll(rememberScrollState()),
-				verticalArrangement = Arrangement.Center,
-				horizontalAlignment = Alignment.CenterHorizontally
-			) {
-				Text(text = stringResource(id = R.string.infoscreen_developer), fontSize = 30.sp)
-				Text(text = "Felix Winkler")
-				Text(text = "Florian Vierkant")
-				Text(text = "Marie Biethahn")
-				Text(text = "Max Henning Junghans")
-				Text(text = "Sebastian Guhl")
-				Text(text = "Steffen Marbach")
-				Spacer(modifier = Modifier.height(30.dp))
-				Text(text = stringResource(id = R.string.infoscreen_support), fontSize = 30.sp)
-				Text(text = stringResource(id = R.string.isp), textAlign = TextAlign.Center)
-				Text(text = stringResource(id = R.string.university))
-				Spacer(modifier = Modifier.height(5.dp))
-				Text(text = "Juljan Bouchagiar")
-				Text(text = "Joshua Wiedekopf")
-				Text(text = "Jan Matyssek")
-				Image(
-					painter = painterResource(id = R.drawable.logo_isp),
-					contentDescription = "ISP Logo"
-				)
-				Spacer(modifier = Modifier.height(30.dp))
-				Text(text = stringResource(id = R.string.infoscreen_license), fontSize = 30.sp)
-				Text(text = stringResource(id = R.string.infoscreen_ownLicense), textAlign = TextAlign.Center)
-				Text(text = stringResource(id = R.string.infoscreen_usingLicense), textAlign = TextAlign.Center)
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopBar(navController, stringResource(id = R.string.ScreenInfo)) },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(30.dp + it.calculateTopPadding())
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.app_name_version, BuildConfig.VERSION_NAME),
+                    fontWeight = FontWeight.Black,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.infoscreen_developer),
+                    fontSize = 30.sp
+                )
+                val developers = listOf(
+                    "Felix Winkler",
+                    "Florian Vierkant",
+                    "Marie Biethahn",
+                    "Max Henning Junghans",
+                    "Sebastian Guhl",
+                    "Steffen Marbach"
+                )
+                developers.forEach { dev ->
+                    Text(dev)
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = stringResource(id = R.string.infoscreen_support), fontSize = 30.sp)
+                Text(text = stringResource(id = R.string.isp), textAlign = TextAlign.Center)
+                Text(text = stringResource(id = R.string.university))
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(text = "Juljan Bouchagiar")
+                Text(text = "Joshua Wiedekopf")
+                Text(text = "Jan Matyssek")
+                Image(
+                    painter = painterResource(id = R.drawable.logo_isp),
+                    contentDescription = "ISP Logo"
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = stringResource(id = R.string.infoscreen_license), fontSize = 30.sp)
+                Text(
+                    text = stringResource(id = R.string.infoscreen_ownLicense),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = stringResource(id = R.string.infoscreen_usingLicense),
+                    textAlign = TextAlign.Center
+                )
 
-				Spacer(modifier = Modifier.height(30.dp))
-				Text(text = "Disclaimer", fontSize = 30.sp)
-				Text(text = stringResource(id = R.string.Disclaimer), textAlign = TextAlign.Center)
-			}
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = "Disclaimer", fontSize = 30.sp)
+                Text(text = stringResource(id = R.string.Disclaimer), textAlign = TextAlign.Center)
+            }
 
-		}
-	)
+        }
+    )
 
 }
